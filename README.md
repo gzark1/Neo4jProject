6d0c1b9b3f375a9ff2d6200d3bc5ff91b5019cf5
marios <t8200003@aueb.gr>
2023-07-08 17:53:41 +0300

    Add images with the result of queries

---
+++ README.md
@@ -0,0 +1,117 @@
+## Neo4j Project
+
+This project is an exaple of the usage of Neo4j graph database.
+
+### Dataset
+
+ A dataset available in the Stanford Large Network Dataset Collection (SNAP). It focuses on Massive Open Online Courses (MOOCs) and contains information about user interactions within these online learning platforms.
+
+MOOCs are online courses that are open to anyone and typically attract a large number of participants. This dataset specifically captures user actions and interactions within MOOCs, providing valuable insights into user behavior and engagement.
+
+The dataset contains 3 files:
+* `mooc_actions.tsv` contains information about user actions within the MOOCs.
+* `mooc_actions_features.tsv` contains information about the features of the user actions.
+* `mooc_actions_labels.tsv` contains information about the labels of the user actions.
+
+### Data Model
+
+The data model is composed of 2 nodes:
+* `User` node, which represents a user.
+* `Target` node, which represents a target (e.g. a video, a quiz, etc.).
+
+The data model is composed of 1 relationship:
+
+* `PERFORMS_ACTION`, which represents the action performed by a user on a target.
+
+
+
+## Queries
+
+### Show a small portion of your graph database (screenshot)
+![img.png](img.png)
+
+### Count all users, count all targets, count all actions
+
+### 1. Count all users
+```
+MATCH (u:User)
+RETURN count(u)
+```
+and the result is 7047
+
+### 2. Count all targets
+```
+MATCH (t:Target)
+RETURN count(t)
+```
+
+and the result is 97
+
+### 3. Count all actions
+```
+MATCH ()-[r]->()
+RETURN count(r) AS actionCount
+
+```
+and the result is 411749
+
+### Show all actions (actionID) and targets (targetID) of a specific user (choose one)
+```
+MATCH (u:User {id: '1'})
+MATCH (u)-[r]->(t:Target)
+RETURN r.id AS actionID, t.id AS targetID
+```
+and the result is
+![img_1.png](img_1.png)
+
+### For each user, count his/her actions
+```
+MATCH (u:User)-[r]->()
+RETURN u.id AS userID, count(r) AS actionCount
+```
+and the result is
+
+![img_2.png](img_2.png)
+
+### For each target, count how many users have done this target
+```
+MATCH (u:User)-[r]->(t:Target)
+RETURN t.id AS targetID, count(DISTINCT u) AS userCount
+```
+and the result is
+![img_3.png](img_3.png)
+
+### Count the average actions per user
+
+```
+MATCH (u:User)
+OPTIONAL MATCH (u)-[r]->()
+WITH u, count(r) AS actionCount
+RETURN avg(actionCount) AS averageActionsPerUser
+```
+and the result is
+![img_5.png](img_5.png)
+
+### Show the userID and the targetID, if the action has positive Feature2
+
+```
+MATCH (u:User)-[r]->(t:Target)
+WHERE toFloat(r.feature2) > 0
+RETURN u.id AS userID, t.id AS targetID
+```
+and the result is:
+![img_6.png](img_6.png)
+
+### For each targetID, count the actions with label “1”
+
+```
+MATCH (u:User)-[r]->(t:Target)
+WHERE r.label = 1
+RETURN t.id AS targetID, count(r) AS actionCount
+```
+and the result is:
+![img_7.png](img_7.png)
+
+#### Authors
+* **Marios Aintini**
+* **Giorgios Zarkadas**
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from neo4j import GraphDatabase
+import pprint
 
 # Replace 'bolt://localhost:7687' with the actual URL of your Neo4j database
 uri = "bolt://localhost:7687"
@@ -9,8 +10,7 @@ database_name = "Graph DBMS"  # Replace with your database name
 # Create the driver with the specified database name
 driver = GraphDatabase.driver(uri, auth=(username, password), database=database_name)
 
-
-#File paths for dataset files
+# File paths for dataset files
 action_features_file = "mooc_action_features.tsv"
 action_labels_file = "mooc_action_labels.tsv"
 actions_file = "mooc_actions.tsv"
@@ -18,55 +18,69 @@ actions_file = "mooc_actions.tsv"
 # Create a Neo4j session
 driver = GraphDatabase.driver(uri, auth=(username, password))
 session = driver.session()
-result = session.run("MATCH (n) RETURN n")
-for record in result:
-    print(record)
-#Function to load action features into Neo4j
+
+
+# load action features
 def load_action_features():
+    actions_features = dict()
+    # Load action features
     with open(action_features_file, 'r') as file:
-        next(file)  # Skip the header line
-        for line in file:
-            action_id, feature0, feature1, feature2, feature3 = line.strip().split('\t')
-            query = """
-            MERGE (a:Action {actionID: $actionID})
-            SET a.feature0 = $feature0, a.feature1 = $feature1, a.feature2 = $feature2, a.feature3 = $feature3
-            """
-            session.run(query, actionID=int(action_id), feature0=float(feature0), feature1=float(feature1),
-                        feature2=float(feature2), feature3=float(feature3))
+        lines = file.readlines()[1:]  # Slice the list to exclude the first line
+        for line in lines:
+            features = line.strip().split('\t')
+            action_id = int(features[0])
+            actions_features[action_id] = features[1:]
+    return actions_features
 
-# Function to load action labels into Neo4j
+
 def load_action_labels():
+    actions_labels = dict()
+    # Load action labels
     with open(action_labels_file, 'r') as file:
-        next(file)  # Skip the header line
-        for line in file:
-            action_id, label = line.strip().split('\t')
-            query = """
-            MATCH (a:Action {actionID: $actionID})
-            SET a.label = $label
-            """
-            session.run(query, actionID=int(action_id), label=int(label))
+        lines = file.readlines()[1:]  # Slice the list to exclude the first line
+        for line in lines:
+            labels = line.strip().split('\t')
+            action_id = int(labels[0])
+            actions_labels[action_id] = int(labels[1])
+    return actions_labels
 
-# Function to load actions into Neo4j
-def load_actions():
+
+def load_dataset():
+    # Load users
+    users = set()
+    targets = set()
+    action_labels = load_action_labels()
+    action_feature = load_action_features()
     with open(actions_file, 'r') as file:
-        next(file)  # Skip the header line
-        for line in file:
-            action_id, user_id, target_id, timestamp = line.strip().split('\t')
-            query = """
-            MATCH (u:User {userID: $userID})
-            MERGE (a:Action {actionID: $actionID})
-            MERGE (t:Target {targetID: $targetID})
-            CREATE (u)-[:PERFORMED {timestamp: $timestamp}]->(a)
-            CREATE (a)-[:TARGETS]->(t)
-            """
-            session.run(query, userID=int(user_id), actionID=int(action_id), targetID=int(target_id),
-                        timestamp=int(timestamp))
+        lines = file.readlines()[1:]  # Slice the list to exclude the first line
+        for line in lines:
+            actions = line.strip().split('\t')
+            action_id = int(actions[0])
+            user_id = actions[1]
+            target_id = actions[2]
+            timestamp = actions[3]
+            # Create a user node if it does not exist
+            if user_id not in users:
+                session.run("CREATE (u:User {id: $id})", id=user_id)
+                users.add(user_id)
+            # Create a target node if it does not exist
+            if target_id not in targets:
+                session.run("CREATE (t:Target {id: $id})", id=target_id)
+                targets.add(target_id)
+            # Create performed action edge between user and target
+            features = action_feature[action_id]
+            # Check if action has label if not set it to -1
+            if action_id in action_labels:
+                label = action_labels[action_id]
+            else:
+                label = -1
+            session.run("MATCH (u:User {id: $user_id}), (t:Target {id: $target_id}) "
+                        "CREATE (u)-[:PERFORMED_ACTION {action_id: $action_id, timestamp: $timestamp,"
+                        " feature0:$feature0, feature1:$feature1, feature2:$feature2, feature3:$feature3,"
+                        " label:$label }]->(t)",
+                        user_id=user_id, target_id=target_id, action_id=action_id, timestamp=timestamp,
+                        feature0=features[0], feature1=features[1], feature2=features[2], feature3=features[3],
+                        label=label)
 
-# Load the data into Neo4j
-load_action_features()
-load_action_labels()
-load_actions()
 
-# Close the Neo4j session
-session.close()
-driver.close()
+load_dataset()
